1122b5a85189561539db70633b9d48c87aab6211
DESKTOP-EIN9P91\User <asct1977ro@gmail.com>
2021-02-17 22:26:02 +0200

    true/false hasCase fix

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 
 import { GuitarService } from '../guitar.service';
 import { Guitar } from '../guitarInterface';
+import { CurrencyService } from '../currency.service';
 
 @Component({
   selector: 'app-details',
@@ -13,7 +14,13 @@ import { Guitar } from '../guitarInterface';
 export class DetailsComponent implements OnInit {
   guitar: Guitar;
   db_key: any;
+  guitars: any[] = [];
+  currency: any;
+
+
+
   constructor(
+    private currencyService: CurrencyService,
     private route: ActivatedRoute,
     private guitarService: GuitarService,
     
@@ -22,16 +29,25 @@ export class DetailsComponent implements OnInit {
     
   }
 
+  
+
   ngOnInit() { 
+    
     this.db_key = this.route.snapshot.paramMap.get('id');
     this.guitarService.getGuitarList().snapshotChanges().subscribe(guitars => {
       guitars.forEach(guitar => {
         if (this.db_key == guitar.payload.key) {
           this.guitar = guitar.payload.val();
+          
         }
       });
     }); 
   }
+
+
+
+
+  
 
 //   updateGuitar(data: any[]) {
 //     this.guitarService.updateGuitar(this.db_key, data);
